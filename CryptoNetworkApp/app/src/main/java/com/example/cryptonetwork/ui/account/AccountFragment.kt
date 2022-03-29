d3b90d55bda808de6a78a2d04c0002f3d8d6a1df
AlenaVasileva <Alenadvasileva@yandex.ru>
2022-03-30 00:16:03 +0300

    git push

---
+++ CryptoNetworkApp/app/src/main/java/com/example/cryptonetwork/ui/account/AccountFragment.kt
@@ -0,0 +1,55 @@
+package com.example.cryptonetwork.ui.account
+
+import android.accounts.Account
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.cryptonetwork.databinding.FragmentAccountBinding
+import com.example.cryptonetwork.ui.login.LoginActivity
+
+class AccountFragment : Fragment() {
+
+    private var _binding: FragmentAccountBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val accountViewModel =
+            ViewModelProvider(this).get(AccountViewModel::class.java)
+
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textAccount
+        accountViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+
+        val loginButton: Button = binding.loginButton
+        loginButton.setOnClickListener {
+            loginButton.text = "New Text"
+            val intent = Intent(context, LoginActivity::class.java).apply {
+                putExtra("keyIdentifier", 1)
+                startActivity(this)
+            }
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ CryptoNetworkApp/app/src/main/java/com/example/cryptonetwork/ui/newsfeed/NewsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.cryptonetwork.ui.newsfeed
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NewsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is news Fragment"
+    }
+    val text: LiveData<String> = _text
+}
